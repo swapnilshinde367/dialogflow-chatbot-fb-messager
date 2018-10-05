bbd7b055a06469bbb295a0c9dc9c4940eb2c4ea7
XCHANGING.MY\Swapnil.Shinde <swapnil.shinde@asia.xchanging.com>
2018-10-05 11:19:40 +0800

    make it better

--- app.js
+++ app.js
@@ -777,9 +777,10 @@ async function greetUserText(userId) {
 		user = usersMap.get(userId);
 	}
 	sendTextMessage(userId, "Welcome " + user.first_name + '! ' +
-		'I can answer frequently asked questions for you. ' +
-		'What can I help you with?');
+		'I can answer frequently asked questions for you ' +
+		'and I perform job interviews. What can I help you with?');
 }
+
 /*
  * Call the Send API. The message data goes in the body. If successful, we'll
  * get the message id in a response
@@ -1001,66 +1002,6 @@ function sendEmail(subject, content) {
 		console.log(response.body)
 		console.log(response.headers)
 	})
-}
-
-function greetUserText(userId) {
-	//first read user firstname
-	request({
-		uri: 'https://graph.facebook.com/v2.7/' + userId,
-		qs: {
-			access_token: config.FB_PAGE_TOKEN
-		}
-	 }, function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			 var user = JSON.parse(body);
-			 if (user.first_name) {
-				console.log("FB user: %s %s, %s",
-					user.first_name, user.last_name, user.gender);
-
-				var pool = new pg.Pool(config.PG_CONFIG);
-				pool.connect(function(err, client, done) {
-					if (err) {
-						return console.error('Error acquiring client', err.stack);
-					}
-					var rows = [];
-					console.log('fetching user');
-					client.query(`SELECT id FROM users WHERE fb_id='${userId}' LIMIT 1`,
-						function(err, result) {
-							console.log('query result ' + result);
-							if (err) {
-								console.log('Query error: ' + err);
-							} else {
-								console.log('rows: ' + result.rows.length);
-								if (result.rows.length === 0) {
-									let sql = 'INSERT INTO users (fb_id, first_name, last_name, profile_pic, ' +
-										'locale, timezone, gender) VALUES ($1, $2, $3, $4, $5, $6, $7)';
-									console.log('sql: ' + sql);
-									client.query(sql,
-										[
-											userId,
-											user.first_name,
-											user.last_name,
-											user.profile_pic,
-											user.locale,
-											user.timezone,
-											user.gender
-										]);
-								}
-							}
-						});
-
-				});
-				pool.end();
-
-				sendTextMessage(userId, "Welcome " + user.first_name + '!');
-			} else {
-				console.log("Cannot get data for fb user with id",
-					userId);
-			}
-		} else {
-			console.error(response.error);
-		}
-	 });
 }
 
 // Spin up the server
