cc5029a1b0d4904032e274a56351d668fbb08078
XCHANGING.MY\Swapnil.Shinde <swapnil.shinde@asia.xchanging.com>
2018-10-05 15:34:02 +0800

    make it better

--- app.js
+++ app.js
@@ -209,7 +209,7 @@ function handleEcho(messageId, appId, metadata) {
 function handleDialogFlowAction(sender, action, messages, contexts, parameters) {
 	switch (action) {
 
-		case "iphone_colors.fovourite":
+		case "iphone_colors.favorite":
 			colors.updateUserColor(parameters.fields['color'].stringValue, sender);
 			let reply = `Oh, I like it, too. I'll remember that.`;
 			sendTextMessage(sender, reply);
--- colors.js
+++ colors.js
@@ -71,6 +71,4 @@ module.exports = {
 		});
 		pool.end();
 	}
-
-
 }
